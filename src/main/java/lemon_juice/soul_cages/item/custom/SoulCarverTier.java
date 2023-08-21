b58f7b3c4d0787adc53dba55e015224783e534ac
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-08-21 19:14:58 -0400

    1.0.2 - Soul Carver Backend Tweak

--- src/main/java/lemon_juice/soul_cages/item/custom/SoulCarverTier.java
+++ src/main/java/lemon_juice/soul_cages/item/custom/SoulCarverTier.java
@@ -1,12 +1,17 @@
 package lemon_juice.soul_cages.item.custom;
 
-
+import lemon_juice.soul_cages.SoulCages;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
 
 public class SoulCarverTier {
-    public static final ForgeTier SOUL_CARVER = new ForgeTier(0, -1, 12.0F, 0.0F, 30, BlockTags.NEEDS_STONE_TOOL, () -> Ingredient.of(Items.AIR));
 
+    public static final Tier SOUL_CARVER = TierSortingRegistry.registerTier(new ForgeTier(0, -1, 12.0F, 0.0F, 30, BlockTags.SWORD_EFFICIENT, () -> Ingredient.of(Items.AIR)), new ResourceLocation(SoulCages.MOD_ID, "soul_carver"), List.of(), List.of());
 }
